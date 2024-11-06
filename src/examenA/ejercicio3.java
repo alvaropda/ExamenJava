be1b05ee649b6e31bc34fcf2c9fec94bce380e7a
alvaropda <alvaropeinadodearcos2002@gmail.com>
2024-11-06 20:24:53 +0100

    actualizado

--- src/examenA/ejercicio3.java
+++ src/examenA/ejercicio3.java
@@ -5,42 +5,22 @@ import java.util.Scanner;
 public class ejercicio3 {
     public static void main(String[] args) {
 
-
         Scanner sc = new Scanner(System.in);
-        System.out.print("dime un nº: ");
         int n = sc.nextInt();
         int suma = 0;
-        int nanterior = 0;
-        boolean flag = true;
 
-        if (n >= 0 || n < 10) {
+        do {
+            System.out.print("dime un nº: ");
+            n = sc.nextInt();
+            suma = 0;
 
-            switch (n) {
-                case 0 -> suma = 0;
-                case 1 -> suma = 1;
-                case 2 -> suma = 2;
-                case 3 -> suma = 3;
-                case 4 -> suma = 4;
-                case 5 -> suma = 5;
-                case 6 -> suma = 6;
-                case 7 -> suma = 7;
-                case 8 -> suma = 8;
-                case 9 -> suma = 9;
+        }while(n < 0);
 
-            }
+        while (n > 0) {
+            suma = suma + n % 10;
+            n = n / 10;
         }
 
-        while (n % 10 != 0 && flag){
-            if (n / 10 > 0) {
-                suma += n % 10;
-                nanterior = n;
-                n = n / 10;
-            }
-            else {
-                suma += nanterior % 10;
-                flag = false;
-            }
-        }
-        System.out.print("la suma de los digitos es: " + suma);
+        System.out.println("La suma de los dígitos es: "+suma);
     }
 }
